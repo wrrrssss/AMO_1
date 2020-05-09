c0c979b78a92a38b7cff1169803418fd93821506
161250152 <161250152@smail.nju.edu.cn>
2020-05-09 18:08:17 +0800

    好友模块修改1

--- src/main/java/com/example/amo1/controller/FriendController.java
+++ src/main/java/com/example/amo1/controller/FriendController.java
@@ -22,41 +22,49 @@ public class FriendController {
     private FriendService friendService;
 
     //新增好友
+    @RequestMapping(value = "/addFriend")
     public ResultMessage addFriend(@RequestBody Friend friend){
         return friendService.addFriend(friend);
     }
 
     //删除好友
+    @RequestMapping(value = "/deleteFriend")
     public ResultMessage deleteFriend(@RequestParam(value = "id")String id){
         return friendService.deleteFriend(Integer.parseInt(id));
     }
 
     //修改好友分组或者备注名信息
+    @RequestMapping(value = "/modifyFriend")
     public ResultMessage modifyFriend(@RequestBody Friend friend){
         return friendService.modifyFriend(friend);
     }
 
     //新增分组 todo 用户创建的时候自动新增一个默认分组
+    @RequestMapping(value = "/addGroup")
     public ResultMessage addGroup(@RequestBody Group group){
         return friendService.addGroup(group);
     }
 
     //修改分组
+    @RequestMapping(value = "/modifyGroup")
     public ResultMessage modifyGroup(@RequestBody Group group){
         return friendService.modifyGroup(group);
     }
 
     //删除分组
+    @RequestMapping(value = "/deleteGroup")
     public ResultMessage deleteGroup(@RequestParam(value = "group_id")String id){
         return friendService.deleteGroup(Integer.parseInt(id));
     }
 
     //获取所有分组
+    @RequestMapping(value = "/getAllGroup")
     public List<Group> getAllGroup(@RequestParam(value = "user_id")String user_id){
         return friendService.getAllGroup(Integer.parseInt(user_id));
     }
 
     //获取某组的所有好友
+    @RequestMapping(value = "/getAllFriend")
     public List<Friend> getAllFriend(@RequestParam(value = "group_id")String group_id){
         return friendService.getAllFriend(Integer.parseInt(group_id));
     }
