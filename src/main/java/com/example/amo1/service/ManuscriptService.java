c9d4629f411cb91219c727f5ad3c0870d0319515
Junfeng Wu <939779131@qq.com>
2020-05-18 18:48:54 +0800

    点赞接口

--- src/main/java/com/example/amo1/service/ManuscriptService.java
+++ src/main/java/com/example/amo1/service/ManuscriptService.java
@@ -1,5 +1,6 @@
 package com.example.amo1.service;
 
+import com.example.amo1.model.LoveData;
 import com.example.amo1.model.Manuscript;
 import com.github.pagehelper.PageInfo;
 
@@ -21,4 +22,9 @@ public interface ManuscriptService {
     //全文检索稿件
     PageInfo<Manuscript> search(Integer currPage, Integer pageSize, String keyword);
 
+    //点赞加一
+    void loveAddOne(LoveData loveData);
+
+    //取消点赞
+    void cancelLove(LoveData loveData);
 }
