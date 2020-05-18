a3b14ebb777d2edda0530dca37d3f45fdcfc8f9c
Junfeng Wu <939779131@qq.com>
2020-05-18 18:53:15 +0800

    点赞表数据

---
+++ src/main/java/com/example/amo1/mapper/LoveDataMapper.java
@@ -0,0 +1,18 @@
+package com.example.amo1.mapper;
+
+import com.example.amo1.model.LoveData;
+import java.util.List;
+
+public interface LoveDataMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(LoveData record);
+
+    LoveData selectByPrimaryKey(Integer id);
+
+    List<LoveData> selectAll();
+
+    int updateByPrimaryKey(LoveData record);
+
+    int deleteByUser(LoveData loveData);
+}
