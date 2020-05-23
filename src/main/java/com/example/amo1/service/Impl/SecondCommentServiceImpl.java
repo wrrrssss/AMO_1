8d6ccd1e77d1bed5300282f8c617bee0e933c087
Junfeng Wu <939779131@qq.com>
2020-05-23 14:10:44 +0800

    日期格式

--- src/main/java/com/example/amo1/service/Impl/SecondCommentServiceImpl.java
+++ src/main/java/com/example/amo1/service/Impl/SecondCommentServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.amo1.service.SecondCommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -16,6 +17,7 @@ public class SecondCommentServiceImpl implements SecondCommentService {
 
     @Override
     public void add(SecondComment secondComment) {
+        secondComment.setCommentdate(new Date());
         secondCommentMapper.insert(secondComment);
     }
 
